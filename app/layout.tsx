6bf1f9da78ea5cc3620ae250f7f828f2817b2d2f
Youjung Lee <youjung.dev@gmail.com>
2024-06-12 21:52:00 +0900

    RegisterModal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/navbar/Navbar";
+import { RegisterModal } from "./components/modals/RegisterModal";
+import { ToasterProvider } from "./providers/ToasterProvider";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -18,6 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
