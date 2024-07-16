d2a838c17a61492888f059d2c7fa7d802f1b37d2
Youjung Lee <youjung.dev@gmail.com>
2024-07-16 18:30:47 +0900

    Swipe on Screen Touch Added.

--- app/become-a-host/[id]/photos/image-modal.tsx
+++ app/become-a-host/[id]/photos/image-modal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { FileWithPreview } from "./image-upload"; // Assuming FileWithPreview type is exported
 import { ChevronLeft, ChevronRight, X } from "lucide-react";
@@ -19,6 +19,7 @@ const ImageModal: React.FC<ImageModalProps> = ({
   onNext,
 }) => {
   const file = files[currentIndex];
+  const [startX, setStartX] = useState(0);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -34,29 +35,54 @@ const ImageModal: React.FC<ImageModalProps> = ({
     };
   }, [currentIndex, onPrev, onNext]);
 
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    setStartX(e.touches[0].clientX);
+  };
+
+  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    if (startX === 0) return;
+    const touchX = e.touches[0].clientX;
+    const diff = touchX - startX;
+
+    if (diff > 50) {
+      onPrev();
+      setStartX(0); // Reset startX to prevent multiple triggers
+    } else if (diff < -50) {
+      onNext();
+      setStartX(0); // Reset startX to prevent multiple triggers
+    }
+  };
+
+  const handleTouchEnd = () => {
+    setStartX(0);
+  };
+
   return (
     <div
       className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
       onClick={onClose}
     >
       <div
-        className="relative h-full w-full bg-white md:h-[680px] md:w-[90vw] md:rounded-xl"
+        className="relative h-full w-full bg-white lg:h-[650px] lg:w-[80vw] lg:rounded-xl"
         onClick={(e) => e.stopPropagation()}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
       >
-        <div className="flex h-full w-full items-center justify-center">
+        <div className="flex h-full w-full items-center justify-center px-7">
           <Image
             src={file.preview}
             width={1000}
             height={1000}
             alt="Full size"
-            className="z-0 mx-auto max-h-[80%] max-w-full object-contain"
+            className="z-0 mx-auto max-h-[80%] max-w-full rounded-xl object-contain lg:rounded-none"
           />
           <p className="absolute bottom-7 right-[5%] line-clamp-1 rounded-full bg-white px-2 py-1 text-sm font-semibold md:text-base">
             {currentIndex + 1} / {files.length}
           </p>
         </div>
         <button
-          className="absolute right-4 top-4 z-10 hover:scale-105"
+          className="absolute right-4 top-4 z-10 transition-all hover:scale-105"
           onClick={onClose}
         >
           <X
@@ -69,7 +95,7 @@ const ImageModal: React.FC<ImageModalProps> = ({
         {currentIndex > 0 && (
           <button
             type="button"
-            className="absolute left-4 top-1/2 z-10 -translate-y-1/2 transform rounded-full hover:bg-white md:left-6"
+            className="absolute left-4 top-1/2 z-10 -translate-y-1/2 transform rounded-full transition-colors hover:bg-white md:left-6"
             onClick={onPrev}
             disabled={currentIndex === 0}
           >
@@ -79,7 +105,7 @@ const ImageModal: React.FC<ImageModalProps> = ({
         {currentIndex < files.length - 1 && (
           <button
             type="button"
-            className="absolute right-4 top-1/2 z-10 -translate-y-1/2 transform rounded-full hover:bg-white md:right-6"
+            className="absolute right-4 top-1/2 z-10 -translate-y-1/2 transform rounded-full transition-colors hover:bg-white md:right-6"
             onClick={onNext}
             disabled={currentIndex === files.length - 1}
           >
